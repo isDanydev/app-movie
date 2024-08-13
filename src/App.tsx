af6cdc94fd07960c77ef11ac0332436efc4bf81f
Danydev <danydveloper@gmail.com>
2024-08-13 14:49:10 -0500

    se corrigio el api_img

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@
 
 import { Footer, Header } from "./components";
 import AllRoutes from "./routes/AllRoutes";
-import { API_KEY, API_URL, API_IMG_CARD } from "../env";
+import { API_KEY, API_URL, IMAGE_URL } from "../env";
 const App = () => {
   // console.log(data);
   // const urlImageTMDB = "https://image.tmdb.org/t/p/w300";
@@ -32,7 +32,7 @@ const App = () => {
       <div>
         <h2>{API_KEY}</h2>
         <h2>{API_URL}</h2>
-        <h2>{API_IMG_CARD}</h2>
+        <h2>{IMAGE_URL}</h2>
       </div>
       <AllRoutes />
       <Footer />
